ed3a3a5904e2312d394a556a49634ad1909925b6
Raza Imam <razaimam9851@gmail.com>
2025-10-18 18:35:15 +0530

    Add sitemap and robots.txt routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', title="Home")
+
+# Sitemap
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'sitemap.xml')
+
+# Robots.txt
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'robots.txt')
 
 # Fitness Blog Pages
 @app.route('/nutrition')
